ba683138e34a7a70fe606c74f77082c5efadd3c3
Harshita Chelani <harshch002@gmail.com>
2026-03-05 22:18:16 +0530

    added assetPrefix for Hostinger CDN

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   //output: "export",
+//   // uncomment befor npm run build
+//   images: {
+//     unoptimized: false,
+//   },
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  //output: "export",
-  // uncomment befor npm run build
+  assetPrefix: "https://jhssi.com",
+
   images: {
     unoptimized: false,
   },
